41cea9c8ede8bc61e894de40e693732f0a6d0502
James Timms <James@reebric.com>
2019-10-20 15:40:19 +0000

    Fixing syntax error with if statement.

--- setup_gcp_sdk.sh
+++ setup_gcp_sdk.sh
@@ -3,9 +3,9 @@
 PYHTON_VERSION=$(python --version)
 GOOGLE_CLOUD_SDK="google-cloud-sdk-267.0.0-linux-x86_64.tar.gz"
 
-if ["$PYTHON_VERSION" == "Python 2.7.5"]
+if ["$PYTHON_VERSION" == "Python 2.7.5"]; then
   echo "Python version $PYTHON_VERSION too old. Need 2.7.9+"
-  exit 0;
+  exit 0
 fi
 
 
